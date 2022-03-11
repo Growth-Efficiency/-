fb56e0bd715d8785b19d4b727f9f88a10aa3c948
JiYoul <wldbf97@naver.com>
2022-03-11 21:40:39 +0900

    스프링 컨테이너, 스프링 빈 학습 및 테스트

--- jiyoul/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ jiyoul/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,14 +4,14 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
-
     private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade().equals(Grade.VIP)) {
             return price * discountPercent / 100;
-        } else {
+        }
+        else {
             return 0;
         }
     }

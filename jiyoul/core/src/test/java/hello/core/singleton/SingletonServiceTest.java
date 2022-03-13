f3a643dda460cce2dc994c302846ff3444fd4f87
JiYoul <wldbf97@naver.com>
2022-03-13 17:00:43 +0900

    싱글톤 컨테이너, 스프링 싱글톤 보장 공부

---
+++ jiyoul/core/src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,16 @@
+package hello.core.singleton;
+
+public class SingletonServiceTest {
+
+    private static final SingletonServiceTest instance = new SingletonServiceTest();
+
+    public static SingletonServiceTest getInstance() {
+        return instance;
+    }
+
+    private SingletonServiceTest() {}
+
+    public static void main(String[] args) {
+        System.out.println("싱글톤 실행");
+    }
+}

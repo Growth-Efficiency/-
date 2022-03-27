33476cb3610c515e94c6a931ede221460791b2e6
dongHyo <kdhyo98@gmail.com>
2022-03-27 22:31:21 +0900

    feat : @Configuration 를 통한 스프링 컨테이너 등록

--- kdhyo/core/src/main/java/hello/core/AppConfig.java
+++ kdhyo/core/src/main/java/hello/core/AppConfig.java
@@ -8,13 +8,18 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
@@ -27,6 +32,5 @@ public class AppConfig {
 //		return new FixDiscountPolicy();
 		return new RateDiscountPolicy();
 	}
-
 
 }
